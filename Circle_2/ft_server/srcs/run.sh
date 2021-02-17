c055d32be1afd9127d9ff5fdcd1327b8b5e4969a
hyunjuyo <hjunyoo17@gmail.com>
2021-02-17 17:11:36 +0900

    ft_server

---
+++ Circle_2/ft_server/srcs/run.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+service nginx start
+service php7.3-fpm start
+service mysql start
+echo "create database if not exists db_1;" | mysql
+echo "create user if not exists 'user_1'@'localhost' identified by '1234567';" | mysql
+echo "grant all privileges on db_1.* to 'user_1'@'localhost' with grant option;" | mysql
+
+bash

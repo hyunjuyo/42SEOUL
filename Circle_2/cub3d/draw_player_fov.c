13521d8395f9a1998144478baed5eeb73bdc9b69
hyunjuyo <hjunyoo17@gmail.com>
2021-03-11 11:53:28 +0900

    cub3d_210311

--- Circle_2/cub3d/draw_player_fov.c
+++ Circle_2/cub3d/draw_player_fov.c
@@ -3,23 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   draw_player_fov.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyunjuyo <hyunjuyo@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: hyunjuyo <hyunjuyo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/10 17:35:11 by hyunjuyo          #+#    #+#             */
-/*   Updated: 2021/03/10 19:15:40 by hyunjuyo         ###   ########.fr       */
+/*   Updated: 2021/03/11 11:50:08 by hyunjuyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int		get_wall_h_pixel(double wdist)
+void	clear_screen(t_game *game)
 {
-	int		fov_vert;
+	int	h;
+	int	w;
+
+	h = 0;
+	while (h < WIN_H)
+	{
+		w = 0;
+		while (w < WIN_W)
+		{
+			game->img1.data[h * WIN_W + w] = BLACK;
+			w++;
+		}
+		h++;
+	}
+}
+
+int		get_wall_h_pixels(double wdist)
+{
+	double	fov_vert;
 	double	h_ratio;
 	int		wall_h;
 
 	fov_vert = deg_to_rad(FOV * WIN_H / WIN_W, 0);
-	h_ratio = 1.0 / wdist * tan(fov_vert / 2.0) * 2.0;
+	h_ratio = 1.0 / (wdist * tan(fov_vert / 2.0) * 2.0);
 	wall_h = (int)(WIN_H * h_ratio);
 	return (wall_h);
 }
@@ -27,25 +45,16 @@ int		get_wall_h_pixel(double wdist)
 void	draw_one_vert_line(int i, double wdist, t_game *game)
 {
 	int	wall_h;
-	int	w_top;
-	int	w_bottom;
-	int	w;
+	int	space;
 	int	h;
 
-	wall_h = get_wall_h_pixel(wdist);
-	printf("wall_h : %d\n", wall_h);
-	w_top = (WIN_H - wall_h) / 2;
-	w_bottom = w_top + wall_h;
-	h = 0;
-	while (h < WIN_H)
+	wall_h = get_wall_h_pixels(wdist);
+	if ((space = (WIN_H - wall_h) / 2) < 0)
+		space = 0;
+	h = space;
+	while (h < WIN_H - space)
 	{
-		w = 0;
-		while (w < WIN_W)
-		{
-			if (w == i && h >= w_top && h <= w_bottom)
-				game->img1.data[h * WIN_W + w] = CYAN;
-			w++;
-		}
+		game->img1.data[h * WIN_W + i] = WHITE;
 		h++;
 	}
 }
@@ -55,14 +64,14 @@ int		draw_player_fov(t_game *game)
 	int		i;
 	double	wdist;
 
+	clear_screen(game);
 	i = 0;
 	while (i < WIN_W)
 	{
 		wdist = cast_single_ray(i, game, WIN_W);
-		printf("wdist : %f\n", wdist);
 		draw_one_vert_line(i, wdist, game);
 		i++;
 	}
-	mlx_put_image_to_window(game->mlx, game->win, game->img1.img, 1, 1);
+	mlx_put_image_to_window(game->mlx, game->win, game->img1.img, 30, 30);
 	return (0);
 }

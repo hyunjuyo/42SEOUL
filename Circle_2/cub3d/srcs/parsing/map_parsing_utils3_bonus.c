24bd48972c3bcbac0a93c78a3419621804e21d8d
hyunjuyo <hjunyoo17@gmail.com>
2021-04-10 13:49:54 +0900

    cub3d_210410

--- Circle_2/cub3d/srcs/parsing/map_parsing_utils3_bonus.c
+++ Circle_2/cub3d/srcs/parsing/map_parsing_utils3_bonus.c
@@ -6,17 +6,18 @@
 /*   By: hyunjuyo <hyunjuyo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 12:47:26 by hyunjuyo          #+#    #+#             */
-/*   Updated: 2021/04/10 13:08:49 by hyunjuyo         ###   ########.fr       */
+/*   Updated: 2021/04/10 13:48:34 by hyunjuyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void    do_check(t_game *game, char *img_path, char *type)
+void	do_check(t_game *game, char *img_path, char *type)
 {
-    t_img   temp;
+	t_img	temp;
 
-	temp.img = mlx_xpm_file_to_image(game->mlx, img_path, &temp.width, &temp.height);
+	temp.img = mlx_xpm_file_to_image(game->mlx, img_path, &temp.width,
+			&temp.height);
 	if (!temp.img)
 	{
 		printf("Error\n[%s]mlx_xpm_file_to_image() failed\n", type);
@@ -24,22 +25,22 @@ void    do_check(t_game *game, char *img_path, char *type)
 	}
 }
 
-void    img_file_open_check(t_game *game)
+void	img_file_open_check(t_game *game)
 {
-    char    *img_path;
+	char	*img_path;
 
-    img_path = game->conf.wall_no;
-    do_check(game, img_path, "NO");
-    img_path = game->conf.wall_so;
-    do_check(game, img_path, "SO");
-    img_path = game->conf.wall_we;
-    do_check(game, img_path, "WE");
-    img_path = game->conf.wall_ea;
-    do_check(game, img_path, "EA");
-    img_path = game->conf.sprite[0];
-    do_check(game, img_path, "S");
-    img_path = game->conf.sprite[1];
-    do_check(game, img_path, "S2");
-    img_path = game->conf.sprite[2];
-    do_check(game, img_path, "S3");
+	img_path = game->conf.wall_no;
+	do_check(game, img_path, "NO");
+	img_path = game->conf.wall_so;
+	do_check(game, img_path, "SO");
+	img_path = game->conf.wall_we;
+	do_check(game, img_path, "WE");
+	img_path = game->conf.wall_ea;
+	do_check(game, img_path, "EA");
+	img_path = game->conf.sprite[0];
+	do_check(game, img_path, "S");
+	img_path = game->conf.sprite[1];
+	do_check(game, img_path, "S2");
+	img_path = game->conf.sprite[2];
+	do_check(game, img_path, "S3");
 }

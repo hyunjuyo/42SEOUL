742204e8ce7363d398b5ee18964bf2bdb9c2d42b
hyunjuyo <hjunyoo17@gmail.com>
2021-06-17 18:37:26 +0900

    Inception_210617

--- Circle_5/Inception/srcs/requirements/mariadb/conf/init.sh
+++ Circle_5/Inception/srcs/requirements/mariadb/conf/init.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 
+service mysql start
+
 echo "create database if not exists db_1;" | mysql
 echo "create user if not exists 'user_1'@'localhost' identified by '1234567';" | mysql
 echo "grant all privileges on db_1.* to 'user_1'@'localhost' with grant option;" | mysql

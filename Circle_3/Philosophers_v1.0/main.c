5e017e23a58070c5b4373a4f82aa7f3e12b53a5f
hyunjuyo <hjunyoo17@gmail.com>
2021-07-09 17:48:59 +0900

    Philosphers_210706

--- Circle_3/Philosophers_v1.0/main.c
+++ Circle_3/Philosophers_v1.0/main.c
@@ -6,20 +6,27 @@
 /*   By: hyunjuyo <hyunjuyo@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 17:55:32 by hyunjuyo          #+#    #+#             */
-/*   Updated: 2021/07/09 17:41:46 by hyunjuyo         ###   ########.fr       */
+/*   Updated: 2021/07/09 17:48:29 by hyunjuyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "philo.h"
 
+int	num;
+
 void	*philo_thread(void *data)
 {
 	struct timeval	cur_time;
+	int	i;
 
 	pthread_mutex_lock(&mutex_lock);
 	gettimeofday(&cur_time, NULL);
 	printf("%ld.%06d %s is eating\n", cur_time.tv_sec, cur_time.tv_usec, (char *)data);
 	usleep(2 * 1000 * 1000);
+	i = -1;
+	while (++i < 10000)
+		num++;
+	printf("%s num : %d\n", (char *)data, num);
 	gettimeofday(&cur_time, NULL);
 	printf("%ld.%06d %s is sleeping\n", cur_time.tv_sec, cur_time.tv_usec, (char *)data);
 	pthread_mutex_unlock(&mutex_lock);
@@ -43,8 +50,12 @@ int	main(int argc, char *argv[])
 	pthread_t		tid3;
 	int				num_of_philos;
 	
+	num = 0;
 	if (argc < 2)
+	{
+		printf("argc is 1\n");
 		return (0);
+	}
 	num_of_philos = ft_atoi(argv[1]);
 	pthread_mutex_init(&mutex_lock, NULL);
 //	tid = create_forks(num_of_philos);
